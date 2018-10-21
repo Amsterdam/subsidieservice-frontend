246a95ea3096611a34abbfed160bf0b435138114
Kalin Marinov <kalin.marinov@auxcode.com>
2018-10-21 17:28:57 +0300

    adding stijil and global styles

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/dashboard">Dashboard</router-link> |
       <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
@@ -10,21 +10,6 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  @import '~amsterdam-stijl/dist/css/ams-stijl.css';
+  @import './assets/styles/main.scss';
 </style>
