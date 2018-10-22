26bd085ee3c4b6dd402f81428ea274da09d57967
Kalin Marinov <kalin.marinov@auxcode.com>
2018-10-22 13:42:05 +0300

    attemting to style things with stijil

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/dashboard">Dashboard</router-link> |
-      <router-link to="/login">Login</router-link>
+    <div id="nav" class="type-nav-primair">
+      <nav class="container">
+        <ul class="links horizontal"> 
+        <li> <a @click="$router.push('/')" > <span class="linklabel"> Home </span>  </a>  </li>
+        <li> <a @click="$router.push('/dashboard')" > <span class="linklabel"> Dashboard </span>  </a>  </li>
+        <li> <a @click="$router.push('/login')" > <span class="linklabel"> Login </span>  </a>  </li>
+        </ul>
+      </nav>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-  @import '~amsterdam-stijl/dist/css/ams-stijl.css';
-  @import './assets/styles/main.scss';
+@import "~amsterdam-stijl/dist/css/ams-stijl.css";
+@import "./assets/styles/main.scss";
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,45 @@
 <template>
-  <div class="home">
+  <div class="container">
     <img alt="Vue logo" src="../assets/images/logo.png">
 
     <h1> Login screen </h1>
 
+
+<div class="formulier-section">
+  <div class="formshadow">
     <form v-on:submit.prevent="login">
-      <input type="text" v-model="username" placeholder="username"> 
+      <!-- <input type="text" v-model="username" placeholder="username"> 
       <input type="password" v-model="password" placeholder="password"> 
-      <button type="submit"> Login </button>
+      <button type="submit"> Login </button> -->
+
+    <div class="rij mode_input text rij_verplicht">
+        <div class="label">
+          <label for="formInput">Username</label>
+        </div>
+
+        <div class="invoer">
+          <input type="text" v-model="username" placeholder="username" class="input">
+        </div>
+      </div>
+      <div class="rij mode_input text rij_verplicht">
+        <div class="label">
+          <label for="formInput">Password</label>
+        </div>
+
+        <div class="invoer">
+          <input  v-model="password" placeholder="password" class="input" type="password">
+        </div>
+      </div>
+
+      <button class="action primary"> Login </button>
 
       <p> {{message}} </p> 
     </form>
+  </div>
+</div>
+
+
+
 
   </div>
 </template>
@@ -29,7 +58,7 @@ export default class Login extends Vue {
 
   async login() {
     if (await userService.login(this.username, this.password)) {
-      this.$router.push('/dashboard');
+      this.$router.push("/dashboard");
     } else {
       this.message = "Login failed";
     }
